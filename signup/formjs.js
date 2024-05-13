ea707a441bdc1554f908a123d7ac3b8b1b3d16aa
MultiMarkTechPro Media <victoroluwadarasimi16@gmail.com>
2024-05-13 15:46:26 +0100

    signup commit

---
+++ signup/formjs.js
@@ -0,0 +1,195 @@
+jQuery(document).ready(function() {
+    // click on next button
+    jQuery('.form-wizard-next-btn').click(function() {
+        var parentFieldset = jQuery(this).parents('.wizard-fieldset');
+        var currentActiveStep = jQuery(this).parents('.form-wizard').find('.form-wizard-steps .active');
+        var next = jQuery(this);
+        var nextWizardStep = true;
+        parentFieldset.find('.wizard-required').each(function(){
+            var thisValue = jQuery(this).val();
+
+            if( thisValue == "") {
+                jQuery(this).siblings(".wizard-form-error").slideDown();
+                nextWizardStep = false;
+            }
+            else {
+                jQuery(this).siblings(".wizard-form-error").slideUp();
+            }
+        });
+        if( nextWizardStep) {
+            next.parents('.wizard-fieldset').removeClass("show","400");
+            currentActiveStep.removeClass('active').addClass('activated').next().addClass('active',"400");
+            next.parents('.wizard-fieldset').next('.wizard-fieldset').addClass("show","400");
+            jQuery(document).find('.wizard-fieldset').each(function(){
+                if(jQuery(this).hasClass('show')){
+                    var formAtrr = jQuery(this).attr('data-tab-content');
+                    jQuery(document).find('.form-wizard-steps .form-wizard-step-item').each(function(){
+                        if(jQuery(this).attr('data-attr') == formAtrr){
+                            jQuery(this).addClass('active');
+                            var innerWidth = jQuery(this).innerWidth();
+                            var position = jQuery(this).position();
+                            jQuery(document).find('.form-wizard-step-move').css({"left": position.left, "width": innerWidth});
+                        }else{
+                            jQuery(this).removeClass('active');
+                        }
+                    });
+                }
+            });
+        }
+    });
+    //click on previous button
+    jQuery('.form-wizard-previous-btn').click(function() {
+        var counter = parseInt(jQuery(".wizard-counter").text());;
+        var prev =jQuery(this);
+        var currentActiveStep = jQuery(this).parents('.form-wizard').find('.form-wizard-steps .active');
+        prev.parents('.wizard-fieldset').removeClass("show","400");
+        prev.parents('.wizard-fieldset').prev('.wizard-fieldset').addClass("show","400");
+        currentActiveStep.removeClass('active').prev().removeClass('activated').addClass('active',"400");
+        jQuery(document).find('.wizard-fieldset').each(function(){
+            if(jQuery(this).hasClass('show')){
+                var formAtrr = jQuery(this).attr('data-tab-content');
+                jQuery(document).find('.form-wizard-steps .form-wizard-step-item').each(function(){
+                    if(jQuery(this).attr('data-attr') == formAtrr){
+                        jQuery(this).addClass('active');
+                        var innerWidth = jQuery(this).innerWidth();
+                        var position = jQuery(this).position();
+                        jQuery(document).find('.form-wizard-step-move').css({"left": position.left, "width": innerWidth});
+                    }else{
+                        jQuery(this).removeClass('active');
+                    }
+                });
+            }
+        });
+    });
+    //click on form submit button
+    jQuery(document).on("click",".form-wizard .form-wizard-submit" , function(){
+        var parentFieldset = jQuery(this).parents('.wizard-fieldset');
+        var currentActiveStep = jQuery(this).parents('.form-wizard').find('.form-wizard-steps .active');
+        parentFieldset.find('.wizard-required').each(function() {
+            var thisValue = jQuery(this).val();
+            if( thisValue == "" ) {
+                jQuery(this).siblings(".wizard-form-error").slideDown();
+            }
+            else {
+                // console.log("Hello");
+                // jQuery(this).siblings(".wizard-form-error").slideUp();
+
+            }
+        });
+    });
+    // focus on input field check empty or not
+    jQuery(".form-control").on('focus', function(){
+        var tmpThis = jQuery(this).val();
+        if(tmpThis == '' ) {
+            jQuery(this).parent().addClass("focus-input");
+        }
+        else if(tmpThis !='' ){
+            jQuery(this).parent().addClass("focus-input");
+        }
+    }).on('blur', function(){
+        var tmpThis = jQuery(this).val();
+        if(tmpThis == '' ) {
+            jQuery(this).parent().removeClass("focus-input");
+            jQuery(this).siblings('.wizard-form-error').slideDown("3000");
+        }
+        else if(tmpThis !='' ){
+            jQuery(this).parent().addClass("focus-input");
+            jQuery(this).siblings('.wizard-form-error').slideUp("3000");
+        }
+    });
+});
+
+
+$(document).on("change", ".uploadProfileInput", function () {
+    var triggerInput = this;
+    var currentImg = $(this).closest(".pic-holder").find(".pic").attr("src");
+    var holder = $(this).closest(".pic-holder");
+    var wrapper = $(this).closest(".profile-pic-wrapper");
+    $(wrapper).find('[role="alert"]').remove();
+    var files = !!this.files ? this.files : [];
+    if (!files.length || !window.FileReader) {
+        return;
+    }
+    if (/^image/.test(files[0].type)) {
+        // only image file
+        var reader = new FileReader(); // instance of the FileReader
+        reader.readAsDataURL(files[0]); // read the local file
+
+        reader.onloadend = function () {
+            $(holder).addClass("uploadInProgress");
+            $(holder).find(".pic").attr("src", this.result);
+            $(holder).append(
+                '<div class="upload-loader"><div class="spinner-border text-primary" role="status"><span class="sr-only">Loading...</span></div></div>'
+            );
+
+            // Dummy timeout; call API or AJAX below
+            setTimeout(() => {
+                $(holder).removeClass("uploadInProgress");
+                $(holder).find(".upload-loader").remove();
+                // If upload successful
+                if (Math.random() < 0.9) {
+                    $(wrapper).append(
+                        '<div class="snackbar show" role="alert"><i class="fa fa-check-circle text-success"></i> Profile image updated successfully</div>'
+                    );
+
+                    // Clear input after upload
+                    $(triggerInput).val("");
+
+                    setTimeout(() => {
+                        $(wrapper).find('[role="alert"]').remove();
+                    }, 3000);
+                } else {
+                    $(holder).find(".pic").attr("src", currentImg);
+                    $(wrapper).append(
+                        '<div class="snackbar show" role="alert"><i class="fa fa-times-circle text-danger"></i> There is an error while uploading! Please try again later.</div>'
+                    );
+
+                    // Clear input after upload
+                    $(triggerInput).val("");
+                    setTimeout(() => {
+                        $(wrapper).find('[role="alert"]').remove();
+                    }, 3000);
+                }
+            }, 1500);
+        };
+    } else {
+        $(wrapper).append(
+            '<div class="alert alert-danger d-inline-block p-2 small" role="alert">Please choose the valid image.</div>'
+        );
+        setTimeout(() => {
+            $(wrapper).find('role="alert"').remove();
+        }, 3000);
+    }
+});
+
+
+
+
+
+
+$(".imgAdd").click(function(){
+    $(this).closest(".row").find('.imgAdd').before('<div class="col-sm-2 imgUp"><div class="imagePreview"></div><label class="btn btn-primary">Upload<input type="file" class="uploadFile img" value="Upload Photo" style="width:0px;height:0px;overflow:hidden;"></label><i class="fa fa-times del"></i></div>');
+});
+$(document).on("click", "i.del" , function() {
+    $(this).parent().remove();
+});
+$(function() {
+    $(document).on("change",".uploadFile", function()
+    {
+        var uploadFile = $(this);
+        var files = !!this.files ? this.files : [];
+        if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support
+
+        if (/^image/.test( files[0].type)){ // only image file
+            var reader = new FileReader(); // instance of the FileReader
+            reader.readAsDataURL(files[0]); // read the local file
+
+            reader.onloadend = function(){ // set image data as background of div
+                //alert(uploadFile.closest(".upimage").find('.imagePreview').length);
+                uploadFile.closest(".imgUp").find('.imagePreview').css("background-image", "url("+this.result+")");
+            }
+        }
+
+    });
+});
+
